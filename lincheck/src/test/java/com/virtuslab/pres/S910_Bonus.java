251a2de893cc7f31d710991895c8ff9bec7fc077
adamw <adam@warski.org>
2026-06-16 13:27:44 +0200

    WIP

---
+++ lincheck/src/test/java/com/virtuslab/pres/S910_Bonus.java
@@ -0,0 +1,16 @@
+package com.virtuslab.pres;
+
+public class S910_Bonus {
+    void _1_jcstress() {
+        /*
+        ℹ️ complementary to fray/lincheck
+        ℹ️ stress testing using forked JVMs
+        ℹ️ small tests with @Actor-s and @Arbiter-s
+        ℹ️ parallelism, not concurrency bugs
+        🍀 catches parallelsim / memory-model bugs (e.g. instruction reordering)
+        🍀 no instrumentation
+        💔 can't reuse existing test cases
+        💔 non-deterministic, no reproduction of bugs
+         */
+    }
+}
